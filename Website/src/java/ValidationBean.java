7290fdfbd117b44f3241a1ee00ea5255ca111f6d
Natasha George <37225550+natashageorge40@users.noreply.github.com>
2018-03-27 17:57:19 -0230

    Created homeQuoteResult

--- Website/src/java/ValidationBean.java
+++ Website/src/java/ValidationBean.java
@@ -15,6 +15,17 @@ public class ValidationBean implements Serializable{
     private String postalCode;
     private String licenseNumber;
     private String licenseDate;
+    private String name2;
+    private String gender2;
+    private String dob2;
+    private String phone2;
+    private String email2;
+    private String address2;
+    private String city2;
+    private String province2;
+    private String postalCode2;
+    private String licenseNumber2;
+    private String licenseDate2;
     private String username;
     private String password;
     private String secondPassword;
@@ -113,7 +124,95 @@ public class ValidationBean implements Serializable{
     public void setLicenseDate(String licenseDate) {
         this.licenseDate = licenseDate;
     }
-    
+
+    public String getName2() {
+        return name2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
+
+    public String getGender2() {
+        return gender2;
+    }
+
+    public void setGender2(String gender2) {
+        this.gender2 = gender2;
+    }
+
+    public String getDob2() {
+        return dob2;
+    }
+
+    public void setDob2(String dob2) {
+        this.dob2 = dob2;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public void setEmail2(String email2) {
+        this.email2 = email2;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity2() {
+        return city2;
+    }
+
+    public void setCity2(String city2) {
+        this.city2 = city2;
+    }
+
+    public String getProvince2() {
+        return province2;
+    }
+
+    public void setProvince2(String province2) {
+        this.province2 = province2;
+    }
+
+    public String getPostalCode2() {
+        return postalCode2;
+    }
+
+    public void setPostalCode2(String postalCode2) {
+        this.postalCode2 = postalCode2;
+    }
+
+    public String getLicenseNumber2() {
+        return licenseNumber2;
+    }
+
+    public void setLicenseNumber2(String licenseNumber2) {
+        this.licenseNumber2 = licenseNumber2;
+    }
+
+    public String getLicenseDate2() {
+        return licenseDate2;
+    }
+
+    public void setLicenseDate2(String licenseDate2) {
+        this.licenseDate2 = licenseDate2;
+    }
+
     public String getUsername() {
         return username;
     }
@@ -121,23 +220,23 @@ public class ValidationBean implements Serializable{
     public void setUsername(String username) {
         this.username = username;
     }
-    
-     public String getPassword() {
+
+    public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
-    
-     public String getSecondPassword() {
+
+    public String getSecondPassword() {
         return secondPassword;
     }
 
     public void setSecondPassword(String secondPassword) {
         this.secondPassword = secondPassword;
     }
-    
+
     public String getVehicleModel() {
         return vehicleModel;
     }
@@ -169,7 +268,7 @@ public class ValidationBean implements Serializable{
     public void setPlateNumber(String plateNumber) {
         this.plateNumber = plateNumber;
     }
-    
+
     public String getHomeYear() {
         return homeYear;
     }
